4002f22507f91c710e95bc31c7bca5284768b142
Jacob Faulks <jcbfaulks@gmail.com>
2024-08-29 20:17:56 -0500

    initialize project

---
+++ lib/elixir_yaml_keys.ex
@@ -0,0 +1,18 @@
+defmodule ElixirYamlKeys do
+  @moduledoc """
+  Documentation for `ElixirYamlKeys`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ElixirYamlKeys.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/elixir_yaml_keys_test.exs
@@ -0,0 +1,8 @@
+defmodule ElixirYamlKeysTest do
+  use ExUnit.Case
+  doctest ElixirYamlKeys
+
+  test "greets the world" do
+    assert ElixirYamlKeys.hello() == :world
+  end
+end
